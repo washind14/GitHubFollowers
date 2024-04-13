0de586b866d88527dd8d216157d15afb9bed368a
Desha Washington <washind14@icloud.com>
2024-04-12 21:32:37 -0600

    Updated UserInfoVC, added childVC

---
+++ GitHubFollowers/CustomViews/Labels/GFSecondaryTitleLabel.swift
@@ -0,0 +1,20 @@
+//
+//  GFSecondaryTitleLabel.swift
+//  GitHubFollowers
+//
+//  Created by Desha Washington on 4/12/24.
+//
+
+import UIKit
+
+class GFSecondaryTitleLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ GitHubFollowers/Utilities/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  GitHubFollowers
+//
+//  Created by Desha Washington on 4/12/24.
+//
+
+import Foundation
